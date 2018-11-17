8cc89fd686988e17d441b1e9be33de8c45eee959
Anastasia Posvystak <ansttss@MacBook-Pro-Anastasia.local>
2018-11-17 21:58:06 +0200

    Babel is installed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,26 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'js/main.js',
+    },
+};
+// ... contents of webpack.config.js
+module.exports = {
+    // ...previous Webpack config...
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
     },
 };
